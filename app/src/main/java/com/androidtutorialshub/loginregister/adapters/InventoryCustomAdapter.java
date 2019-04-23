db204f2994ad83367b7eee9c8a1df83bc280767a
Bara, Ionut <ibara@deloittece.com>
2019-04-23 23:06:21 +0300

    add custom search bar in order to search the equipments by name

--- app/src/main/java/com/androidtutorialshub/loginregister/adapters/InventoryCustomAdapter.java
+++ app/src/main/java/com/androidtutorialshub/loginregister/adapters/InventoryCustomAdapter.java
@@ -5,41 +5,49 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import com.androidtutorialshub.loginregister.R;
 import com.androidtutorialshub.loginregister.model.Equipment;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 /**
- * Created by Madalina Bara on 2619-Apr-19.
+ * Created by Madalina Bara on 2019-Apr-19.
  */
-public class InventoryCustomAdapter extends BaseAdapter {
+public class InventoryCustomAdapter extends BaseAdapter implements Filterable {
 
     private Context context;
     private List<Equipment> inventoryList;
+    private List<Equipment> filteredList;
+    private EquipmentFilter filter;
+
 
     public InventoryCustomAdapter(Context context, List<Equipment> userModelArrayList) {
         this.context = context;
         this.inventoryList = userModelArrayList;
+        this.filteredList = inventoryList;
+        getFilter();
     }
 
 
     @Override
     public int getCount() {
-        return inventoryList.size();
+        return filteredList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return inventoryList.get(position);
+        return filteredList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
@@ -62,14 +70,69 @@ public class InventoryCustomAdapter extends BaseAdapter {
             holder = (ViewHolder) convertView.getTag();
         }
 
-        holder.tvname.setText(String.format("Name: %s", inventoryList.get(position).getName()));
-        holder.tvroom.setText(String.format("Room: %s", inventoryList.get(position).getRoom()));
+        holder.tvname.setText(String.format("Name: %s", filteredList.get(position).getName()));
+        holder.tvroom.setText(String.format("Room: %s", filteredList.get(position).getRoom()));
         //holder.tvcity.setText("City: "+inventoryList.get(position).getCity());
         return convertView;
     }
 
+    /**
+     * Get custom filter
+     *
+     * @return filter
+     */
+    @Override
+    public Filter getFilter() {
+        if (filter == null) {
+            filter = new EquipmentFilter();
+        }
+        return filter;
+    }
+
     private class ViewHolder {
         TextView tvname, tvroom;
+    }
+
+    /**
+     * Custom filter for equipments list
+     * Filter content in equipments list according to the search text
+     */
+    private class EquipmentFilter extends Filter {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults filterResults = new FilterResults();
+            if (constraint != null && constraint.length() > 0) {
+                List<Equipment> tempList = new ArrayList<>();
+
+                // search content in inventory list
+                for (Equipment equipment : inventoryList) {
+                    if (equipment.getName().toLowerCase().contains(constraint.toString().toLowerCase())) {
+                        tempList.add(equipment);
+                    }
+                }
+                filterResults.count = tempList.size();
+                filterResults.values = tempList;
+            } else {
+                filterResults.count = inventoryList.size();
+                filterResults.values = inventoryList;
+            }
+
+            return filterResults;
+        }
+
+        /**
+         * Notify about filtered list to ui
+         *
+         * @param constraint text
+         * @param results    filtered result
+         */
+        @SuppressWarnings("unchecked")
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            filteredList = (ArrayList<Equipment>) results.values;
+            notifyDataSetChanged();
+        }
     }
 
 }
